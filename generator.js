d3817d08367fc231c8d0294e701b20d04fa578bd
Zach <zahngol@gmail.com>
2020-02-12 16:59:07 -0500

    Random number display

--- generator.js
+++ generator.js
@@ -1,4 +1,11 @@
 function generate()
 {
-    document.getElementById("content").innerHTML = "Zach is learning";
+    let result = getRandomInt(0,10);
+    document.getElementById("content").innerHTML = result.toString();
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
