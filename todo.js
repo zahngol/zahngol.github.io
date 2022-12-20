7677f4b11c0261ce11a3d5e71165de8d13f31926
ZP <zahngol@gmail.com>
2022-12-20 11:02:36 -0600

    Completed initial todo list from tutorial

--- todo.js
+++ todo.js
@@ -7,10 +7,10 @@ const uncompletedTodosDiv = document.querySelector('.uncompleted-todos')
 window.onload = () => {
     let storageTodoItems = localStorage.getItem('todoItems')
     if (storageTodoItems !== null) {
-        todoItems = JSON.parse(storageTodoItems);
+        todoItems = JSON.parse(storageTodoItems)
     }
 
-    render();
+    render()
 }
 
 todoInput.onkeyup = ((e) => {
@@ -30,19 +30,38 @@ function addTodo(text) {
         completed: false
     })
 
-    console.log(todoItems)
+    saveAndRender()
 }
 
 function removeTodo(id) {
-
+    todoItems = todoItems.filter(todo => todo.id !== Number(id))
+    saveAndRender()
 }
 
 function markAsCompleted(id) {
+    todoItems = todoItems.filter(todo => {
+        if(todo.id === Number(id)){
+            todo.completed = true;
+        }
 
+        return todo
+    })
+
+    // audio.play()
+
+    saveAndRender()
 }
 
 function markAsUncompleted(id) {
+    todoItems = todoItems.filter(todo => {
+        if(todo.id === Number(id)){
+            todo.completed = false;
+        }
 
+        return todo
+    })
+ 
+    saveAndRender()
 }
 
 function save() {
@@ -50,7 +69,27 @@ function save() {
 }
 
 function render() {
+    let uncompletedTodos = todoItems.filter(item => !item.completed)
+    let completedTodos = todoItems.filter(item => item.completed)
 
+    completedTodosDiv.innerHTML = ''
+    uncompletedTodosDiv.innerHTML = ''
+
+    if (uncompletedTodos.length > 0) {
+        uncompletedTodos.forEach(todo => {
+            uncompletedTodosDiv.append(createTodoElement(todo))
+        })
+    }
+    else {
+        uncompletedTodosDiv.innerHTML = `<div class='empty'>No incomplete tasks</div>`
+    }
+
+    if (completedTodos.length > 0) {
+        completedTodosDiv.innerHTML = `<div class='completed-title'>Completed (${completedTodos.length}/${todoItems.length})</div>`
+        completedTodos.forEach(todo => {
+            completedTodosDiv.append(createTodoElement(todo))
+        })
+    }
 }
 
 function saveAndRender() {
@@ -59,5 +98,36 @@ function saveAndRender() {
 }
 
 function createTodoElement(todo) {
+    const todoDiv = document.createElement('div')
+    todoDiv.setAttribute('data-id', todo.id)
+    todoDiv.className = 'todo-item'
 
+    const todoTextSpan = document.createElement('span')
+    todoTextSpan.innerHTML = todo.text
+
+    const todoInputCheckbox = document.createElement('input')
+    todoInputCheckbox.type = 'checkbox'
+    todoInputCheckbox.checked = todo.completed
+    todoInputCheckbox.onclick = (e) => {
+        let id = e.target.closest('.todo-item').dataset.id
+        e.target.checked ? markAsCompleted(id) : markAsUncompleted(id)
+    }
+
+    const todoRemoveButton = document.createElement('a')
+    todoRemoveButton.href = '#'
+    todoRemoveButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                                </svg>`
+    todoRemoveButton.onclick = (e) => {
+        let id = e.target.closest('.todo-item').dataset.id
+        removeTodo(id)
+    }
+
+    todoTextSpan.prepend(todoInputCheckbox)
+    todoDiv.appendChild(todoTextSpan)
+    todoDiv.appendChild(todoRemoveButton)
+
+    return todoDiv
 }
